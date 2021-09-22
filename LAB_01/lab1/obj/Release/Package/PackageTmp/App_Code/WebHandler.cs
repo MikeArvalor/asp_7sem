800d6a918ca4b525ee77911c0d13931c82d27bf6
Mihail Dubrovksy <43237977+MikeArvalor@users.noreply.github.com>
2021-09-22 12:32:57 +0300

    asp

---
+++ LAB_01/lab1/obj/Release/Package/PackageTmp/App_Code/WebHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.SessionState;
+
+namespace Lab_1.App_Code {
+
+    public class WebHandler : IHttpHandler, IRequiresSessionState {
+
+        public static int result = 0;
+
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context) {
+            if (context.Session["stack"] == null) {
+                Stack<int> EmptyStack = new Stack<int>();
+                context.Session.Add("stack", EmptyStack); 
+            }
+
+            Stack<int> stack = (Stack<int>)context.Session["stack"];
+            switch (context.Request.HttpMethod) {
+
+                case "GET": {
+                    context.Response.Write(JsonConvert.SerializeObject(new {
+                        result = result + stack.FirstOrDefault()
+                    }));
+                    break;
+                };
+
+                case "POST": {
+                    result = Convert.ToInt32(context.Request.Params["result"]);
+                    break;
+                };
+
+                case "PUT": {
+                    stack.Push(Convert.ToInt32(context.Request.Params["add"]));
+                    break;
+                };
+
+                case "DELETE": {
+                    if (stack.Count != 0) {
+                        stack.Pop();
+                    }
+                    break;
+                };
+            }
+
+            context.Response.AddHeader("access-control-allow-origin", "*");
+            context.Response.AddHeader("Access-Control-Allow-Methods", "*");
+        }
+    }
+
+}
